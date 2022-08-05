ea736b48ba016398918d80f1ab32133d3376c856
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2022-08-05 09:33:42 -0400

    update database

--- server/db.py
+++ server/db.py
@@ -1,4 +1,11 @@
 from cs50 import SQL
+import os
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///cs50Final.db")
+# Configure CS50 Library to use SQLite database (for local testing)
+# db = SQL("sqlite:///cs50Final.db")
+
+# Configure CS50 Library to use Heroku Postgres
+uri = os.getenv("DATABASE_URI")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://")
+db = SQL(uri)
