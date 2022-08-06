71d4664e408f42fc1942ac08fed0948af509b7bc
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2022-08-05 20:15:09 -0400

    Clean up the handle error method

--- server/helpers.py
+++ server/helpers.py
@@ -2,17 +2,7 @@
 from flask import redirect, render_template, request, session
 
 def handle_error(message, code=400):
-    """Render message as an apology to user."""
-    def escape(s):
-        """
-        Escape special characters.
-
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
-        return s
-    return render_template("error.html", top=code, bottom=escape(message)), code
+    """Render message as an apology to user. Adapted from Finance PSET."""
+    return render_template("error.html", code=code, message=message)
 
 
